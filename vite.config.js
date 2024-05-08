14a603fe72b3c54c13e3e9d12a74436bc959b7d2
limit1219c@gmail.com <limit1219c@gmail.com>
2024-05-08 14:22:39 +0800

    add http header, coop, coep

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import cp from 'vite-plugin-cp';
+
 
 export default defineConfig({
     base: '/node-web/',
+
+    build: {
+        outDir: 'dist',
+        target: 'esnext',
+    },
+
+    plugins: [
+        cp({
+            targets: [
+                { src: 'node_modules/coi-serviceworker/coi-serviceworker.js', dest: 'dist' }
+            ]
+        })
+    ]
 });
